7836e90561e82687c0d79cc5a6fe8423396b7eb5
Stefan Spataro <stefanspataro@hotmail.com>
2024-05-19 21:01:42 -0700

    structured development repository

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,13 @@
 //server file
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+
 const app = express();
-const port = process.env.PORT;
+app.use(bodyParser.json());
+
+const port = 80;
 
 app.get('/', (req, res) => {
   res.send('Hello from the backend!');
@@ -11,3 +16,21 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Backend server is running at http://localhost:${port}`);
 });
+
+const connection = mysql.createConnection({
+  host: 'mysql',  // Hostname
+  user: 'root',   // Username
+  password: 'rootpw',   // Password
+  database: 'cpdsolution', // Database name
+  port: 3306
+})
+
+connection.connect((err) => {
+  if(err){
+    console.error('error connection: ', err);
+    return;
+  }
+  console.log('connected to database');
+})
+
+connection.end();
